2f1ebab5984779bfd17902509c121a8c2e0149d3
Jay Dave <jayd72059@gmail.com>
2022-01-31 23:09:40 +0530

    day4 completed

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,11 +9,47 @@ class LoginPage extends StatelessWidget {
         Image.asset(
           "assests/images/login_image.png",
           fit: BoxFit.cover,
+          height: 300,
+          width: 250,
+          color: Colors.white70.withOpacity(0.2),
+          colorBlendMode: BlendMode.softLight,
+        ),
+        SizedBox(
+          height: 12,
         ),
         Text(
           "Welcome my friend",
           style: TextStyle(
-              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black38),
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: "Username", labelText: "Enter Username"),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Password", labelText: "Enter Password"),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                style: TextButton.styleFrom(),
+                child: Text("Login"),
+                onPressed: () {
+                  print("hey you are logged into the website");
+                },
+              )
+            ], //children
+          ),
         )
       ],
     ));
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'home_page.dart';
 
@@ -13,7 +14,10 @@ class myapp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       initialRoute: "/login",
       routes: {
         "/": (context) => LoginPage(),
