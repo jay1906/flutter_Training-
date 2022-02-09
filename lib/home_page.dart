8ca756e653355b2a73f20fafe8dd2cb4b1b87b63
Jay Dave <jayd72059@gmail.com>
2022-02-09 19:12:35 +0530

    day12 completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/Models/catalog.dart';
 import 'package:my_app/Widgets/Drawer.dart';
+import 'package:my_app/Widgets/item_widget.dart';
 
 // ignore: camel_case_types
 class HomePage extends StatelessWidget {
   final int Days = 30;
   @override
   Widget build(BuildContext context) {
+    final dummylist = List.generate(10, (index) => Catalogmodel.items[0]);
     return Scaffold(
         appBar: AppBar(
           title: Text(
             'CatalogApp',
           ),
         ),
-        body: Center(
-          child: Container(
-            child: Text(
-              'this is a tutorial application for just practising for $Days days',
-              style: TextStyle(
-                  color: Colors.blueAccent,
-                  fontSize: 23,
-                  fontWeight: FontWeight.w400),
-            ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemCount: dummylist.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: dummylist[index]);
+            },
           ),
         ),
         drawer: MyDrawer());
